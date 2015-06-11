e2dbe1e724bfe27a30e0a620aec42110af581506
aimee <aimee@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-06-11 09:02:22 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@40 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-service/src/main/java/com/simi/vo/sec/SecVo.java
@@ -0,0 +1,15 @@
+package com.simi.vo.sec;
+
+import com.simi.po.model.sec.Sec;
+
+public class SecVo extends Sec{
+	private String imgUrlNew;
+
+	public String getImgUrlNew() {
+		return imgUrlNew;
+	}
+
+	public void setImgUrlNew(String imgUrlNew) {
+		this.imgUrlNew = imgUrlNew;
+	}
+}
