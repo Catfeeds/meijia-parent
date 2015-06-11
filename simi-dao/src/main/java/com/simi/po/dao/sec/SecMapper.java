af4cf492f18ab9a78639cc1eb0535ecbb28c6122
aimee <aimee@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-06-11 09:00:31 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@37 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-dao/src/main/java/com/simi/po/dao/sec/SecMapper.java
@@ -0,0 +1,25 @@
+package com.simi.po.dao.sec;
+
+import java.util.HashMap;
+import java.util.List;
+
+import com.simi.po.model.sec.Sec;
+
+
+public interface SecMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Sec record);
+
+    int insertSelective(Sec record);
+
+    Sec selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(Sec record);
+
+    int updateByPrimaryKey(Sec record);
+    
+    List<Sec> selectByListPage();
+
+	Sec selectByNameAndOtherId(HashMap map);
+}
