49d4a50e10014d7e16308689cd46f7a9a56cd3b6
aimee <aimee@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-06-11 09:02:34 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@41 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-service/src/main/java/com/simi/service/sec/SecService.java
@@ -0,0 +1,17 @@
+package com.simi.service.sec;
+import com.github.pagehelper.PageInfo;
+import com.simi.po.model.sec.Sec;
+
+
+public interface SecService {
+
+	 int insertSelective(Sec record);
+
+	 PageInfo searchVoListPage(int pageNo, int pageSize);
+	 
+	 int deleteByPrimaryKey(Long id);
+
+	 Sec initSec();
+	
+	 Sec selectByNameAndOtherId(String name, Long id);
+}
