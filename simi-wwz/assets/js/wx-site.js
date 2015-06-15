3d486e9f550e9e9b0e2a2de30d08595b6691b2c7
leon <leon@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-06-15 07:06:50 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@81 52ac4245-7803-4e87-b018-04b7c5ce731e

--- simi-wwz/assets/js/wx-site.js
+++ simi-wwz/assets/js/wx-site.js
@@ -1,41 +1,7 @@
-//var siteAPIPath = "http://localhost:8080/onecare/app/"; //本地
-//var localUrl = "http://localhost:8080";
-var localUrl = "http://www.yougeguanjia.com";
-var siteAPIPath = "http://www.yougeguanjia.com/onecare/app/"; //正式
-//var siteAPIPath = "http://182.92.237.135/onecare/app/";  //测试
-
-var cities = {1:'',2:'北京',3:'天津'};
-var weekDay = {0:'周日',1:'周一',2:'周二',3:'周三',4:'周四',5:'周五',6:'周六'};
-var holidays = ['2015-2-17','2015-2-18','2015-2-19','2015-2-20','2015-2-21','2015-2-22','2015-2-23','2015-2-24',
-                '2015-2-25','2015-2-26','2015-2-27','2015-2-28','2015-4-5','2015-5-1'];
-var serviceDict = {1:{'name':'做饭','image':'images/index-zuofan.png','key':'zuofan'},
-                   2:{'name':'洗衣','image':'images/index-xiyi.png','key':'xiyi'},
-                   3:{'name':'家电清洗','image':'images/index-jiadian.png','key':'jiadian'},
-                   4:{'name':'保洁','image':'images/index-baojie.png','key':'baojie'},
-                   5:{'name':'擦玻璃','image':'images/index-caboli.png','key':'caboli'},
-                   6:{'name':'管道疏通','image':'images/index-guandao.png','key':'guandao'},
-                   7:{'name':'新居开荒','image':'images/index-xinju.png','key':'xinju'}};
-
-var infoTypeDict = {1:{'name':'INFO_NAME_1','image':''},
-                    2:{'name':'INFO_NAME_3','image':''},
-                    3:{'name':'INFO_NAME_3','image':''}};
-
-var baseData={'baojie':{'2':'401', '3':'402', '4':'403'},
-              'zuofan':{'2':'101', '3':'102',' 4':'103', '5':'104'},
-              'caboli':{'8':'501'},//算
-              'xinju':{},//算
-              'guandao':{},//算
-              'jiadian':[['301','空调','空调描述',150,150,'台'],
-                         ['302','油烟机(中式)','油烟机(中式)',160,160,'台'],
-                         ['303','油烟机(欧式)','油烟机(欧式)',200,200,'台'],
-                         ['304','微波炉','微波炉',100,100,'台'],
-                         ['305','洗衣机','洗衣机',120,120,'台'],
-                         ['306','电冰箱(双门)','电冰箱(双门)',120,120,'台'],
-                         ['307','电冰箱(三门)','电冰箱(三门)',160,160,'台'],
-                         ['308','电冰箱(对开门)','电冰箱(对开门)',180,180,'台'],
-                         ['309','烤箱','烤箱',100,100,'台']
-                        ]
-             };
+var host = window.location.host;
+var appName = "simi-oa";
+var localUrl = "http://" + host;
+var siteAPIPath = localUrl + "/" + appName; //正式
 
 // $(function(){
     // if(supports_html5_storage()){}
@@ -63,82 +29,7 @@ function changeAddr(f,g){
     return;
 }
 
-//载入用户地址
-function loadAddr(){
-    if(localStorage['user_address']=="undefined" || typeof localStorage['user_address']==="undefined" || localStorage['user_address']==''){
-        //请求用户地址
-        $.ajax({
-            type : "GET",
-            url  : siteAPIPath+"user/get_addrs.json",
-            dataType: "json",
-            cache : false,
-            data : {"mobile":localStorage['user_phone']},
-            success : onUserAddrSuccess,
-            error : onUserAddrError
-        });
-    }
-    else{
-        var pageName = getPageName();
-        var go = pageName[0].split('-')[1];
-        var o={"userAddr":cities[localStorage['user_city_id']]+' '+localStorage['user_address'],
-               "funCall":"changeAddr('true','"+go+"')",
-               "buttonText":"更换",
-               "disabled":""
-              };
-        displayAddr(o);
-    }
-}
 
-function displayAddr(o)
-{
-    var userCell='<label for="user-address">服务地址:</label>\
-                    <div class="am-input-group">\
-                        <input type="text" class="am-form-field" value=\"'+o.userAddr+'\" '+o.disabled+'>\
-                        <span class="am-input-group-btn">\
-                            <button type="button" class="am-btn am-btn-warning" onclick="'+o.funCall+';">'+o.buttonText+'</button>\
-                        </span>\
-                    </div>';
-    $('#user_cell').html(userCell);
-}
-
-//用户地址处理
-function onUserAddrSuccess(data, status){
-  if(data.status != "0"){
-    if (data.status =="999")
-        alert(data.msg);
-    else
-        alert("未能获取用户地址，请稍后再试。");
-    return;
-  }
-
-  //记录用户常用地址
-  if(data.data[0]){
-      localStorage['user_address'] = data.data[0].cell_name + data.data[0].addr;
-      localStorage['user_addr_id'] = data.data[0].addr_id;//小区id
-      localStorage['user_city_id'] = data.data[0].city_id;
-      var pageName = getPageName();
-      var go = pageName[0].split('-')[1];
-      var o={"userAddr":cities[localStorage['user_city_id']]+' '+localStorage['user_address'],
-             "funCall":"changeAddr('true','"+go+"')",
-             "buttonText":"更换",
-             "disabled":""};
-     displayAddr(o);
-  }
-  else{//没有地址
-    var pageName = getPageName();
-    var go = pageName[0].split('-')[1];
-    var o={"userAddr":"您需要先添加地址",
-           "funCall" :"changeAddr('false','"+go+"')",
-           "buttonText":"添加地址",
-           "disabled":"disabled"
-           };
-    displayAddr(o);
-  }
-}
-function onUserAddrError(data, status){
-    //console.log(data.msg);
-    alert("未能获取用户地址，请稍后再试。(5)");
-}
 function getPageName()
 {
     var a = location.href;
