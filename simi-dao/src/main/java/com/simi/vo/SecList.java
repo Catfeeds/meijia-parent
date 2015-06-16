e31affb6fd84d326ace5bc88b9cbe73ef49e3428
aimee <aimee@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-06-16 05:56:46 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@92 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-dao/src/main/java/com/simi/vo/SecList.java
@@ -0,0 +1,63 @@
+package com.simi.vo;
+
+public class SecList {
+	    private Long id;
+	    
+	    private String name;
+
+	    private String mobile;
+
+	    private String nickName;
+	    private Long cityId;
+	    
+	    private String headImg;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getMobile() {
+			return mobile;
+		}
+
+		public void setMobile(String mobile) {
+			this.mobile = mobile;
+		}
+
+		public String getNickName() {
+			return nickName;
+		}
+
+		public void setNickName(String nickName) {
+			this.nickName = nickName;
+		}
+
+		public Long getCityId() {
+			return cityId;
+		}
+
+		public void setCityId(Long cityId) {
+			this.cityId = cityId;
+		}
+
+		public String getHeadImg() {
+			return headImg;
+		}
+
+		public void setHeadImg(String headImg) {
+			this.headImg = headImg;
+		}
+	    
+}
