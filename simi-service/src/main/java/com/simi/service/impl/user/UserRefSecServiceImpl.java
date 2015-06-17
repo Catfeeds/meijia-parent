9f8aa0fdab9e1feb3f4a2d6220fbbc1e22e6390c
aimee <aimee@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-06-17 03:25:40 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@106 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-service/src/main/java/com/simi/service/impl/user/UserRefSecServiceImpl.java
@@ -0,0 +1,33 @@
+package com.simi.service.impl.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.simi.po.dao.user.UserRefSecMapper;
+import com.simi.po.model.user.UserRefSec;
+import com.simi.service.user.UserRefSecService;
+import com.simi.vo.user.UserViewVo;
+
+@Service
+public class UserRefSecServiceImpl implements UserRefSecService{
+  
+	@Autowired
+	private UserRefSecMapper userRefSecMapper;
+	
+	@Override
+	public List<UserViewVo> selectVoByUserId(Long userId) {
+		
+		List<UserViewVo> list=userRefSecMapper.selectVoByUserId(userId);
+		
+		return list;
+	}
+
+	@Override
+	public UserRefSec selectBySecId(Long secId) {
+		
+		return userRefSecMapper.selectBySecId(secId);
+	}
+
+}
