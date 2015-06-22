ba651eddb34a9d19a817788809d3ac29e7e107ce
aimee <aimee@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-06-22 07:10:50 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@200 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-service/src/main/java/com/simi/vo/sec/SecCeShi.java
@@ -0,0 +1,20 @@
+package com.simi.vo.sec;
+
+public class SecCeShi {
+	
+private Long secId;
+private String mobile;
+public Long getSecId() {
+	return secId;
+}
+public void setSecId(Long secId) {
+	this.secId = secId;
+}
+public String getMobile() {
+	return mobile;
+}
+public void setMobile(String mobile) {
+	this.mobile = mobile;
+}
+
+}
