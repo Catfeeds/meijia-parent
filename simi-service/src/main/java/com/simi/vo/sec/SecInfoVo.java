2bf987571623def374aa2b887246bbe9f03c7cf0
aimee <aimee@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-06-22 01:49:28 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@174 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-service/src/main/java/com/simi/vo/sec/SecInfoVo.java
@@ -0,0 +1,17 @@
+package com.simi.vo.sec;
+
+import com.simi.po.model.sec.Sec;
+
+public class SecInfoVo extends Sec{
+	 
+private String cityName;
+
+public String getCityName() {
+	return cityName;
+}
+
+public void setCityName(String cityName) {
+	this.cityName = cityName;
+}
+
+}
