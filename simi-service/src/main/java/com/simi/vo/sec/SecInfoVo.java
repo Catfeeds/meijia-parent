e163f2844eb4b486d4bc7e261152fde87aecc5d2
aimee <aimee@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-06-23 04:11:12 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@225 52ac4245-7803-4e87-b018-04b7c5ce731e

--- simi-service/src/main/java/com/simi/vo/sec/SecInfoVo.java
+++ simi-service/src/main/java/com/simi/vo/sec/SecInfoVo.java
@@ -1,90 +1,106 @@
 package com.simi.vo.sec;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
 public class SecInfoVo {
 	
-private Long id;
-private String mobile;
-private String name;
-private String sex;
-private String headImg;
-private Date birthDay;
-private Long cityId;
-private String cityName;
-private Long addTime;
 
-public Long getId() {
-	return id;
-}
+	private Long id;
+	private String mobile;
+	private String name;
+	private String nickName;
+	private String sex;
+	private String headImg;
+	private String birthDay;
+	private Long cityId;
+	private String cityName;
+	private Long addTime;
 
-public void setId(Long id) {
-	this.id = id;
-}
+	public Long getId() {
+		return id;
+	}
 
-public String getMobile() {
-	return mobile;
-}
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-public void setMobile(String mobile) {
-	this.mobile = mobile;
-}
+	public String getMobile() {
+		return mobile;
+	}
 
-public String getName() {
-	return name;
-}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
 
-public void setName(String name) {
-	this.name = name;
-}
+	public String getName() {
+		return name;
+	}
 
-public String getSex() {
-	return sex;
-}
+	public void setName(String name) {
+		this.name = name;
+	}
 
-public void setSex(String sex) {
-	this.sex = sex;
-}
 
-public String getHeadImg() {
-	return headImg;
-}
 
-public void setHeadImg(String headImg) {
-	this.headImg = headImg;
-}
+	public String getNickName() {
+		return nickName;
+	}
 
-public Date getBirthDay() {
-	return birthDay;
-}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
 
-public void setBirthDay(Date birthDay) {
-	this.birthDay = birthDay;
-}
+	public String getSex() {
+		return sex;
+	}
 
-public Long getCityId() {
-	return cityId;
-}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
 
-public void setCityId(Long cityId) {
-	this.cityId = cityId;
-}
+	public String getHeadImg() {
+		return headImg;
+	}
 
-public Long getAddTime() {
-	return addTime;
-}
+	public void setHeadImg(String headImg) {
+		this.headImg = headImg;
+	}
 
-public void setAddTime(Long addTime) {
-	this.addTime = addTime;
-}
 
-public String getCityName() {
-	return cityName;
-}
 
-public void setCityName(String cityName) {
-	this.cityName = cityName;
-}
+	public String getBirthDay() {
+		return birthDay;
+	}
 
-}
+	public void setBirthDay(String birthDay) {
+		this.birthDay = birthDay;
+	}
+
+	public Long getCityId() {
+		return cityId;
+	}
+
+	public void setCityId(Long cityId) {
+		this.cityId = cityId;
+	}
+
+	public Long getAddTime() {
+		return addTime;
+	}
+
+	public void setAddTime(Long addTime) {
+		this.addTime = addTime;
+	}
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+
+	}
+
