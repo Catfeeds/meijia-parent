d3b4613858d8df2485495079f07623c042ecd572
kerry <kerry@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-06-27 03:32:33 +0000

    --保存秘书信息
    git-svn-id: svn://123.57.173.36/meijia-parent@302 52ac4245-7803-4e87-b018-04b7c5ce731e

--- simi-f7/js/sec/mine-info-edit.js
+++ simi-f7/js/sec/mine-info-edit.js
@@ -1,8 +1,11 @@
+var secMobile,secId;
 myApp.onPageInit('mine-info-edit', function(page) {
 
+	
+	secMobile = localStorage['sec_mobile'];
+	secId = localStorage['sec_id'];
 	// 获取城市信息列表
 	getCityList();
-
 });
 function cityListSuccess(data, textStatus, jqXHR) {
 	var result = JSON.parse(data.response);
@@ -15,15 +18,14 @@ function cityListSuccess(data, textStatus, jqXHR) {
 	$$.each(cityList,function(i,item){
 		citySelected.append('<option value="'+item.city_id+'">'+item.name+'</option>');
 	});
-	getSecInfo(secId, mobile);
+	getSecInfo(secId, secMobile);
 }
 function getCityList() {
-	
 	$$.ajax({
 		type : "GET",
 		url : siteAPIPath + "city/get_list.json?",
 		dataType : "json",
-		cache : false,
+		cache : true,
 		statusCode : {
 				200 : cityListSuccess,
 				400 : ajaxError,
@@ -31,33 +33,31 @@ function getCityList() {
 			}
 	});
 }
-
-function getSecInfo(secId, mobile) {
-	var secInfoSuccess = secInfoSuccess(data, status) {
-		var result = JSON.parse(data.response);
-		if (result.status == "999") {
-			myApp.alert(result.msg);
-			return;
-		}
-		var sec = result.data;
-		$$("#name").val(sec.name);
-		$$("#nickName").val(sec.nick_name);
-		if(sec.sex !=' ' && sec.sex !=null){
-			$("#sex option[value=" + sec.sex + "]").attr("selected", true);
-		}
-		$$("#birthDay").val(sec.birth_day);
-		$$("#city_name option[value="+sec.city_id+"]").attr("selected", true);
-		$$("#secId").val(sec.id);
+function secInfoSuccess(data, textStatus, jqXHR) {
+	var result = JSON.parse(data.response);
+	if (result.status == "999") {
+		myApp.alert(result.msg);
+		return;
 	}
-	
+	var sec = result.data;
+	$$("#name").val(sec.name);
+	$$("#nickName").val(sec.nick_name);
+	if(sec.sex !=' ' && sec.sex !=null){
+		$$("#sex option[value=" + sec.sex + "]").attr("selected", true);
+	}
+	$$("#birthDay").val(sec.birth_day);
+	//$$("#city_name option[value="+sec.city_id+"]").attr("selected", true);
+	$$("#secId").val(sec.id);
+}
+function getSecInfo(secId, mobile) {
 	var postdata = {};
 	postdata.sec_id = secId;
 	postdata.mobile = secMobile;
-	$.ajax({
+	$$.ajax({
 		type : "POST",
 		url : siteAPIPath + "sec/get_secinfo.json",
 		dataType : "json",
-		cache : false,
+		cache : true,
 		data : postdata,
 		statusCode : {
 			200 : secInfoSuccess,
@@ -68,22 +68,31 @@ function getSecInfo(secId, mobile) {
 }
 //保存秘书信息
 
-$("#mind_info_submit").bind("click", function() {
-	var saveSecSuccess = saveSecSuccess(data, status) {
-		var result = JSON.parse(data.response);
-		if (result.status == "999") {
-			myApp.alert(result.msg);
-			return;
-		}
-		window.location.href = "mine-info.html";
-	} 
+function saveSecSuccess(data, textStatus, jqXHR) {
+	var result = JSON.parse(data.response);
+	if (result.status == "999") {
+		myApp.alert(result.msg);
+		return;
+	}
+	window.location.href = "mine-info.html";
+} 
+
+
+$$("#mind_info_submit").click(function() {
+	myApp.alert("kk");
+console.log("kk");
 	
-	/*var $form = $('#order-form');
+});
+/*
+$$("#mind_info_submit").on("click", function() {
+	myApp.alert("kk");
+	
+	var $form = $('#order-form');
 	var isValid = $('.am-form').data('amui.validator').isFormValid();
 	if (!isValid) {
 		alert("is false");
 		return ;
-	}*/
+	}
 	var secId = localStorage['sec_id'];
 	var name = $$("#name").val();
 	var nickName = $$("#nickName").val();
@@ -111,6 +120,6 @@ $("#mind_info_submit").bind("click", function() {
 			500 : ajaxError
 		}
 	});
-})
+})*/
 
 
