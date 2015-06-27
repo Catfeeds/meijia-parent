72b16d64f98988d5f387b7efc53c99661e3a9a0d
leon <leon@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-06-27 12:00:07 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@314 52ac4245-7803-4e87-b018-04b7c5ce731e

--- simi-f7/js/order/order-form.js
+++ simi-f7/js/order/order-form.js
@@ -1,50 +1,108 @@
 myApp.onPageBeforeInit('order-form-page', function(page) {
 
 	var userId = page.query.user_id;
-
+	
+	var orderNo = page.query.order_no;
+	
+	var orderId = 0;
+	
 	if (userId == undefined || userId == '' || userId == 0) {
 		return;
 	}
-
+	
 	var secId = localStorage['sec_id'];
+	
+	//如果为新增订单
+	if (orderNo == undefined || orderNo == '' || orderNo == 0) {
+	
+		var userInfoSuccess = function(data, textStatus, jqXHR) {
+			
+			// We have received response and can hide activity indicator
+			myApp.hideIndicator();
+			var result = JSON.parse(data.response);
+			var user = result.data;
+			var formData = {
+				'sec_id' : secId,
+				'user_id' : userId,
+				'name' : user.name,
+				'mobile' : user.mobile
+			}
+	
+			myApp.formFromJSON('#order-form', formData);
+			console.log($$("#sec_id").val());
+	
+		};
+	
+		var postdata = {};
+	
+		postdata.user_id = userId;
+		$$.ajax({
+			type : "GET",
+			url : siteAPIPath + "user/get_userinfo.json",
+			dataType : "json",
+			cache : true,
+			data : postdata,
+	
+			statusCode : {
+				200 : userInfoSuccess,
+				400 : ajaxError,
+				500 : ajaxError
+			},
+			success : function() {
+	
+			}
+		});
+	} else {  	
+		//如果为修改订单
+		
+		var orderInfoSuccess = function(data, textStatus, jqXHR) {
+			
+			// We have received response and can hide activity indicator
+			myApp.hideIndicator();
+			var result = JSON.parse(data.response);
+			var order = result.data;
+			var formData = {
+				'sec_id' 			: secId,
+				'user_id' 			: userId,
+				'name' 				: order.name,
+				'mobile' 			: order.mobile,
+				'service_type'		: order.service_type,
+				'service_content' 	: order.service_content,
+				'order_pay_type'	: order.order_pay_type,
+				'order_money'		: order.order_money
+			}
+	
+			myApp.formFromJSON('#order-form', formData);
+			
+			orderId = order.id;
+	
+		};
+		
+		var postdata = {};
+	
+		postdata.user_id = userId;
+		postdata.order_no = orderNo;
+		$$.ajax({
+			type : "GET",
+			url : siteAPIPath + "order/get_detail.json",
+			dataType : "json",
+			cache : true,
+			data : postdata,
+			statusCode : {
+				200 : orderInfoSuccess,
+				400 : ajaxError,
+				500 : ajaxError
+			},
+			success : function() {
+	
+			}
+		});		
+		
+		
+	}
 
-	var userInfoSuccess = function(data, textStatus, jqXHR) {
-		// We have received response and can hide activity indicator
-		myApp.hideIndicator();
-		var result = JSON.parse(data.response);
-		var user = result.data;
-		var formData = {
-			'sec_id' : secId,
-			'user_id' : userId,
-			'name' : user.name,
-			'mobile' : user.mobile
-		}
-
-		myApp.formFromJSON('#order-form', formData);
-		console.log($$("#sec_id").val());
-
-	};
-
-	var postdata = {};
-
-	postdata.user_id = userId;
-	$$.ajax({
-		type : "GET",
-		url : siteAPIPath + "user/get_userinfo.json",
-		dataType : "json",
-		cache : true,
-		data : postdata,
-
-		statusCode : {
-			200 : userInfoSuccess,
-			400 : ajaxError,
-			500 : ajaxError
-		},
-		success : function() {
-
-		}
-	});
-
+	
+	
 	$$('#order-submit').on('click', function() {
 
 		
@@ -67,18 +125,40 @@ myApp.onPageBeforeInit('order-form-page', function(page) {
 
 		};		
 		
-		var formData = myApp.formToJSON('#order-form');
-		$$.ajax({
-			cache : true,
-			type : "POST",
-			url : siteAPIPath + "order/post_add.json",
-			data : formData,
-			statusCode : {
-				200 : orderSubmitSuccess,
-				400 : ajaxError,
-				500 : ajaxError
-			},
-		});
+		//如果为新增订单
+		if (orderNo == undefined || orderNo == '' || orderNo == 0) {
+			var formData = myApp.formToJSON('#order-form');
+			$$.ajax({
+				cache : true,
+				type : "POST",
+				url : siteAPIPath + "order/post_add.json",
+				data : formData,
+				statusCode : {
+					200 : orderSubmitSuccess,
+					400 : ajaxError,
+					500 : ajaxError
+				},
+			});
+		} else {
+			//修改订单
+			
+			var postdata = {};
+			
+			postdata.user_id = userId;
+			postdata.order_id = orderId;
+			
+			$$.ajax({
+				cache : true,
+				type : "POST",
+				url : siteAPIPath + "order/push_order.json",
+				data : postdata,
+				statusCode : {
+					200 : orderSubmitSuccess,
+					400 : ajaxError,
+					500 : ajaxError
+				},
+			});
+		}
 	});
 
 });
