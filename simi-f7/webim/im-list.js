b59dff55fdde20a9320ada89ebb0c7a53ea1a9db
sunzhen <sunzhen@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-06-30 08:43:23 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@336 52ac4245-7803-4e87-b018-04b7c5ce731e

--- simi-f7/webim/im-list.js
+++ simi-f7/webim/im-list.js
@@ -1,23 +1,23 @@
-myApp.onPageInit('chat', function (page) {
+// myApp.onPageInit('chat', function (page) {
 
-    var im_username = localStorage['im_username'];
-    var im_password = localStorage['im_password'];
-    var userId = page.query.user_id;
+//     var im_username = localStorage['im_username'];
+//     var im_password = localStorage['im_password'];
+//     var userId = page.query.user_id;
     
-    console.log("im_username = " + im_username);
-    console.log("im_passowrd = " + im_password);
-    console.log("userId = " + userId);
+//     console.log("im_username = " + im_username);
+//     console.log("im_passowrd = " + im_password);
+//     console.log("userId = " + userId);
     
-    var clientWidth = document.body.scrollWidth ;
-    var clientHeight = document.body.scrollHeight ;
-    console.log(document.body.clientWidth);
-    console.log(document.body.clientHeight);
+//     var clientWidth = document.body.scrollWidth ;
+//     var clientHeight = document.body.scrollHeight ;
+//     console.log(document.body.clientWidth);
+//     console.log(document.body.clientHeight);
     
-    var html = '<iframe src="js/lib/webim/index.html" id="iframe" frameborder="0" scrolling="auto" class="mainiframe" width="100%" height="'+clientHeight+'"></iframe>';
-    $$('.page-content-inner').append(html);
+//     var html = '<iframe src="js/lib/webim/index.html" id="iframe" frameborder="0" scrolling="auto" class="mainiframe" width="100%" height="'+clientHeight+'"></iframe>';
+//     $$('.page-content-inner').append(html);
 
 
-});
+// });
 
 //列表页
 myApp.onPageBeforeInit('im-list-page', function (page) {
