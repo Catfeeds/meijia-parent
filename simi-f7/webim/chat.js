59f6ff85bcdaa055d5fc49e3367a44cf5b8121be
sunzhen <sunzhen@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-07-01 08:51:07 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@394 52ac4245-7803-4e87-b018-04b7c5ce731e

--- simi-f7/webim/chat.js
+++ simi-f7/webim/chat.js
@@ -28,6 +28,65 @@ myApp.template7Data['page:messages'] = function(){
         var result = {};
         var imID        = localStorage['im_username'];
         var imPWD       = localStorage['simi-sec-2'];
+
+
+        conn = new Easemob.im.Connection();
+        //初始化连接
+        conn.init({
+            https : false,
+            //当连接成功时的回调方法
+            onOpened : function() {
+                handleOpen(conn);
+            },
+            //当连接关闭时的回调方法
+            onClosed : function() {
+                handleClosed();
+            },
+            //收到文本消息时的回调方法
+            onTextMessage : function(message) {
+                handleTextMessage(message);
+            },
+            //收到表情消息时的回调方法
+            onEmotionMessage : function(message) {
+                handleEmotion(message);
+            },
+            //收到图片消息时的回调方法
+            onPictureMessage : function(message) {
+                handlePictureMessage(message);
+            },
+            //收到音频消息的回调方法
+            onAudioMessage : function(message) {
+                handleAudioMessage(message);
+            },
+            //收到位置消息的回调方法
+            onLocationMessage : function(message) {
+                handleLocationMessage(message);
+            },
+            //收到文件消息的回调方法
+            onFileMessage : function(message) {
+                handleFileMessage(message);
+            },
+            //收到视频消息的回调方法
+            onVideoMessage : function(message) {
+                handleVideoMessage(message);
+            },
+            //收到联系人订阅请求的回调方法
+            onPresence : function(message) {
+                handlePresence(message);
+            },
+            //收到联系人信息的回调方法
+            onRoster : function(message) {
+                handleRoster(message);
+            },
+            //收到群组邀请时的回调方法
+            onInviteMessage : function(message) {
+                handleInviteMessage(message);
+            },
+            //异常时的回调方法
+            onError : function(message) {
+                handleError(message);
+            }
+        });
         
 
         
