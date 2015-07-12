8df641c5c53cec08cc839e6638b70b5a63e4478f
sunzhen <sunzhen@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-07-12 12:44:41 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@455 52ac4245-7803-4e87-b018-04b7c5ce731e

--- simi-f7/webim/im-list.js
+++ simi-f7/webim/im-list.js
@@ -51,6 +51,7 @@ myApp.template7Data['page:user-list-page'] = function(){
 //列表页初始化函数
 myApp.onPageInit('im-list-page', function (page) {
         webim.curroster = null;
+        console.log('init im list and 执行红点函数');
         webim.newMessageDot();
 
 });
