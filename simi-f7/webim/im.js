c0dce411b3cbe122fd6ab3d01077f69ace984c0b
sunzhen <sunzhen@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-07-12 12:12:16 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@447 52ac4245-7803-4e87-b018-04b7c5ce731e

--- simi-f7/webim/im.js
+++ simi-f7/webim/im.js
@@ -204,8 +204,8 @@ webim = {
 
 
 	                  	//未读数量
-	                  	//this.noreadFlag[from] = this.noreadFlag[from] || 0;
-	                  	console.log(this.noreadFlag[from]);
+	                  	this.noreadFlag[from] = this.noreadFlag[from] || 0;
+	                  	// console.log(this.noreadFlag[from]);
 
 
 	                  	console.log(message);
