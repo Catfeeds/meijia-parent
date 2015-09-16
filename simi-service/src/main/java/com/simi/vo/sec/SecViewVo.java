58326febcdbc7cd3ca5003dbd9168ab6d1c83521
leon <leon@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-09-16 13:05:43 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@528 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-service/src/main/java/com/simi/vo/sec/SecViewVo.java
@@ -0,0 +1,97 @@
+package com.simi.vo.sec;
+
+public class SecViewVo {
+	
+
+    private Long secId;
+
+    private String mobile;
+    
+    private String provinceName;
+
+    private String name;
+    
+    private String sex;
+    
+    private	String headImg;
+
+    private Short userType;
+    
+    private String imUserName;
+    
+    private String description;
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getProvinceName() {
+		return provinceName;
+	}
+
+	public void setProvinceName(String provinceName) {
+		this.provinceName = provinceName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getHeadImg() {
+		return headImg;
+	}
+
+	public void setHeadImg(String headImg) {
+		this.headImg = headImg;
+	}
+
+	public Short getUserType() {
+		return userType;
+	}
+
+	public void setUserType(Short userType) {
+		this.userType = userType;
+	}
+
+	public String getImUserName() {
+		return imUserName;
+	}
+
+	public void setImUserName(String imUserName) {
+		this.imUserName = imUserName;
+	}
+
+	public Long getSecId() {
+		return secId;
+	}
+
+	public void setSecId(Long secId) {
+		this.secId = secId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
+
