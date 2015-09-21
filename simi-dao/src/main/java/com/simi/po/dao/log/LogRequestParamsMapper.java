4cd000e86798949fe0cd194c1f25cbf0a149ee84
leon <leon@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-09-21 08:27:13 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@540 52ac4245-7803-4e87-b018-04b7c5ce731e

--- simi-dao/src/main/java/com/simi/po/dao/log/LogRequestParamsMapper.java
+++
@@ -1,9 +0,0 @@
-package com.simi.po.dao.log;
-
-import com.simi.po.model.log.LogRequestParams;
-
-public interface LogRequestParamsMapper {
-    int insert(LogRequestParams record);
-
-    int insertSelective(LogRequestParams record);
-}
