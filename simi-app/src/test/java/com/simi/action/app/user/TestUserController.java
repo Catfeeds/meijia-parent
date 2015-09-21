2926fe785b7ce58d684a973ddd3395c5a2184773
leon <leon@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-09-21 08:28:16 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@543 52ac4245-7803-4e87-b018-04b7c5ce731e

--- simi-app/src/test/java/com/simi/action/app/user/TestUserController.java
+++ simi-app/src/test/java/com/simi/action/app/user/TestUserController.java
@@ -77,7 +77,7 @@ public class TestUserController extends JUnitActionBase  {
     public void testGetUserInfo() throws Exception {
 
 		String url = "/app/user/get_userinfo.json";
-		String params = "?user_id=1";
+		String params = "?user_id=95";
 		MockHttpServletRequestBuilder getRequest = get(url + params);
 
 
