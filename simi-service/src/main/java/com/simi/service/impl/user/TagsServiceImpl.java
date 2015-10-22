c0e4f47d5137058c959708c3240271a147f0bc12
aimee <aimee@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-10-22 10:15:31 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@578 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-service/src/main/java/com/simi/service/impl/user/TagsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.simi.service.impl.user;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.simi.po.dao.user.TagUsersMapper;
+import com.simi.po.dao.user.TagsMapper;
+import com.simi.po.model.user.TagUsers;
+import com.simi.po.model.user.Tags;
+import com.simi.service.user.TagsService;
+import com.simi.service.user.TagsUsersService;
+import com.sun.javadoc.Tag;
+@Service
+public class TagsServiceImpl implements TagsUsersService {
+
+	@Autowired
+	private TagUsersMapper tagUsersMapper;
+
+	@Override
+	public int insertByTagUsers(TagUsers tagUsers) {
+		
+		return tagUsersMapper.insertSelective(tagUsers);
+	}
+	
+}
