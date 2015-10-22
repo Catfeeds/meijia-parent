3e9a998173242996f758c3884e4f139ab9c19aa2
aimee <aimee@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-10-22 10:16:22 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@582 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-dao/src/main/java/com/simi/po/dao/user/TagsMapper.java
@@ -0,0 +1,22 @@
+package com.simi.po.dao.user;
+
+import java.util.List;
+
+import com.simi.po.model.user.Tags;
+import com.sun.javadoc.Tag;
+
+public interface TagsMapper {
+    int deleteByPrimaryKey(Long tagId);
+
+    int insert(Tags record);
+
+    int insertSelective(Tags record);
+
+    Tags selectByPrimaryKey(Long tagId);
+
+    int updateByPrimaryKeySelective(Tags record);
+
+    int updateByPrimaryKey(Tags record);
+
+	List<Tags> selectAll();
+}
