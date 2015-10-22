efe02b1646a3ab7ea21b27283e12302b347a8ed9
aimee <aimee@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-10-22 10:15:46 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@580 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-service/src/main/java/com/simi/service/user/TagsService.java
@@ -0,0 +1,15 @@
+package com.simi.service.user;
+
+import java.util.List;
+
+import com.simi.po.model.user.Tags;
+import com.sun.javadoc.Tag;
+
+
+public interface TagsService {
+
+	List<Tags> selectAll();
+
+	
+
+}
---
+++ simi-service/src/main/java/com/simi/service/user/TagsUsersService.java
@@ -0,0 +1,12 @@
+package com.simi.service.user;
+
+import com.simi.po.model.user.TagUsers;
+
+
+public interface TagsUsersService {
+
+	int insertByTagUsers(TagUsers tagUsers);
+
+	
+
+}
