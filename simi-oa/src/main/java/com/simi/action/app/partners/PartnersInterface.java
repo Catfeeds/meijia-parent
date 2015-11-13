8e619c861409cc7e31f45cd1aeb31a3af30028fe
aimee <aimee@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-11-13 09:02:22 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@718 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-oa/src/main/java/com/simi/action/app/partners/PartnersInterface.java
@@ -0,0 +1,45 @@
+package com.simi.action.app.partners;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.simi.action.BaseController;
+import com.simi.common.ConstantMsg;
+import com.simi.common.Constants;
+import com.simi.po.model.partners.Partners;
+import com.simi.service.partners.PartnersService;
+import com.simi.vo.AppResultData;
+
+@Controller
+@RequestMapping(value = "/interface-partners")
+public class PartnersInterface extends BaseController {
+
+
+	@Autowired
+	private PartnersService partnersService;
+	
+	/**
+	 * 根据公司名称进行排重
+	 * @return
+	 */
+	@SuppressWarnings("rawtypes")
+	@RequestMapping(value = "check-companyName-dumplicate", method = RequestMethod.GET)
+    public AppResultData<List> getRegions(
+    		
+    		@RequestParam(value = "companyName") String companyName) {
+
+		List<Partners> listPartners = partnersService.selectByCompanyName(companyName);
+
+		AppResultData<List> result = new AppResultData<List>(
+				Constants.SUCCESS_0, ConstantMsg.SUCCESS_0_MSG, listPartners);
+
+    	return result;
+    }
+
+
+}
