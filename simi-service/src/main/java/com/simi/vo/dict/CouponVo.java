d80799ae6c5772103151f01dea45a7795cdb0151
aimee <aimee@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-11-14 09:35:04 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@728 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-service/src/main/java/com/simi/vo/dict/CouponVo.java
@@ -0,0 +1,21 @@
+package com.simi.vo.dict;
+
+import com.simi.po.model.dict.DictCoupons;
+
+
+public class CouponVo extends DictCoupons{
+
+	private String expTimeStr;
+
+	public String getExpTimeStr() {
+		return expTimeStr;
+	}
+
+	public void setExpTimeStr(String expTimeStr) {
+		this.expTimeStr = expTimeStr;
+	}
+	
+	
+
+
+}
