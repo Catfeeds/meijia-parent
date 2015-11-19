23b5cd9a7a7d1b54bb545cd61f278d9152421d0d
aimee <aimee@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-11-19 09:09:38 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@790 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-app/src/main/java/com/simi/action/app/user/UserDetailPayController.java
@@ -0,0 +1,52 @@
+package com.simi.action.app.user;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.simi.action.app.BaseController;
+import com.simi.common.ConstantMsg;
+import com.simi.common.Constants;
+import com.simi.po.model.user.UserDetailPay;
+import com.simi.po.model.user.UserDetailScore;
+import com.simi.service.user.UserDetailPayService;
+import com.simi.vo.AppResultData;
+
+@Controller
+@RequestMapping(value = "/app/user")
+public class UserDetailPayController extends BaseController {
+
+
+	@Autowired
+	private UserDetailPayService userDetailPayService;
+
+	/*
+	 * 用户消费明细接口
+	 * 接口文档: http://182.92.160.194/trac/wiki/%E6%88%91%E7%9A%84%E4%BC%98%E6%83%A0%E5%88%B8%E6%8E%A5%E5%8F%A3
+	 * @param
+	 *  user_id 用户ID
+	 */
+	@RequestMapping(value = "get_detail_pay", method = RequestMethod.GET)
+	public AppResultData<List<UserDetailPay>> getDetailPay(
+			@RequestParam("user_id") Long userId,
+			@RequestParam(value = "page", required = false, defaultValue = "1") int page
+			) {
+		
+		List<UserDetailPay> userAddrs = userDetailPayService.selectByUserIdPage(
+				userId, page);
+		if (userAddrs != null && userAddrs.size() > 0) {
+			AppResultData<List<UserDetailPay>> result = new AppResultData<List<UserDetailPay>>(
+					Constants.SUCCESS_0, ConstantMsg.SUCCESS_0_MSG, userAddrs);
+			return result;
+		} else {
+			AppResultData<List<UserDetailPay>> result = new AppResultData<List<UserDetailPay>>(
+					Constants.ERROR_999, ConstantMsg.ERROR_999_MSG_10,
+					new ArrayList<UserDetailPay>());
+			return result;
+		}
+}
+}
---
+++ simi-app/src/test/java/com/simi/action/app/user/TestUserDetailPayController.java
@@ -0,0 +1,44 @@
+package com.simi.action.app.user;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.simi.action.app.JUnitActionBase;
+
+public class TestUserDetailPayController extends JUnitActionBase {
+
+	
+	/**
+	 * 用户消费明细接口
+	 * @throws Exception
+	 */
+	@Test
+	public void testGetDetailPay() throws Exception {
+
+		String url = "/app/user/get_detail_pay.json";
+		String params = "?user_id=77";
+		MockHttpServletRequestBuilder getRequest = get(url + params);
+
+		ResultActions resultActions = this.mockMvc.perform(getRequest);
+		resultActions.andExpect(content().contentType(this.mediaType));
+		resultActions.andExpect(status().isOk());
+
+		System.out.println("RestultActons: " + resultActions.andReturn().getResponse().getContentAsString());
+
+	}
+}
