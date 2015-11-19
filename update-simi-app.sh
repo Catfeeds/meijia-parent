0cb1c32f751b6b56e8d2b81da478f83a555710b1
leon <leon@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-11-19 08:51:19 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@788 52ac4245-7803-4e87-b018-04b7c5ce731e

--- update-simi-app.sh
+++ update-simi-app.sh
@@ -1,6 +1,5 @@
 cd /data/src/meijia-parent/
 svn up
-cd /data/src/meijia-parent/simi-app
 mvn clean package
 /data/tomcat/bin/shutdown.sh
 sleep 1
--- update-simi-oa.sh
+++ update-simi-oa.sh
@@ -1,9 +1,8 @@
 cd /data/src/meijia-parent/
 svn up
-cd /data/src/meijia-parent/simi-oa
 mvn clean package
 /data/tomcat/bin/shutdown.sh
 sleep 1
 rm -rf /data/tomcat/webapps/simi-oa
-\cp -rf /data/src/meijia-parent/simi-oa/target/simi.war /data/tomcat/webapps/
+\cp -rf /data/src/meijia-parent/simi-oa/target/simi-oa.war /data/tomcat/webapps/
 /data/tomcat/bin/startup.sh
