df4d4070d8e55d0987249f84a5e7008deb025803
aimee <aimee@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-11-26 06:44:26 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@827 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-service/src/main/java/com/simi/vo/order/OrdersListVo.java
@@ -0,0 +1,71 @@
+package com.simi.vo.order;
+
+import java.math.BigDecimal;
+
+import com.simi.po.model.order.Orders;
+
+public class OrdersListVo extends Orders{
+	
+	private String ServiceTypeName;
+	
+	private String addr;
+	
+	private String partnerUserName;
+	
+	private String orderStatusName;
+	
+	private BigDecimal orderMoney;
+
+	private BigDecimal orderPay;
+
+	public String getServiceTypeName() {
+		return ServiceTypeName;
+	}
+
+	public void setServiceTypeName(String serviceTypeName) {
+		ServiceTypeName = serviceTypeName;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+	public String getPartnerUserName() {
+		return partnerUserName;
+	}
+
+	public void setPartnerUserName(String partnerUserName) {
+		this.partnerUserName = partnerUserName;
+	}
+
+	public String getOrderStatusName() {
+		return orderStatusName;
+	}
+
+	public void setOrderStatusName(String orderStatusName) {
+		this.orderStatusName = orderStatusName;
+	}
+
+	public BigDecimal getOrderMoney() {
+		return orderMoney;
+	}
+
+	public void setOrderMoney(BigDecimal orderMoney) {
+		this.orderMoney = orderMoney;
+	}
+
+	public BigDecimal getOrderPay() {
+		return orderPay;
+	}
+
+	public void setOrderPay(BigDecimal orderPay) {
+		this.orderPay = orderPay;
+	}
+
+
+
+}
