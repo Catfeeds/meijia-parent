daee44286efa06a7e1959087128cdc8bc5dff942
aimee <aimee@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-11-26 09:33:38 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@831 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-service/src/main/java/com/simi/service/impl/xcloud/XcompanyDeptServiceImpl.java
@@ -0,0 +1,12 @@
+package com.simi.service.impl.xcloud;
+
+import org.springframework.stereotype.Service;
+
+import com.simi.service.xcloud.XcompanyDeptService;
+
+@Service
+public class XcompanyDeptServiceImpl implements XcompanyDeptService {
+	
+		
+
+}
---
+++ simi-service/src/main/java/com/simi/service/xcloud/XcompanyDeptService.java
@@ -0,0 +1,5 @@
+package com.simi.service.xcloud;
+
+public interface XcompanyDeptService {
+
+}
