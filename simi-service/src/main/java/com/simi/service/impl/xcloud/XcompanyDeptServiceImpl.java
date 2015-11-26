a67a3da28bc7164435d6766c8e1b5aac575aed8a
leon <leon@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-11-26 09:54:54 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@832 52ac4245-7803-4e87-b018-04b7c5ce731e

--- simi-service/src/main/java/com/simi/service/impl/xcloud/XcompanyDeptServiceImpl.java
+++ simi-service/src/main/java/com/simi/service/impl/xcloud/XcompanyDeptServiceImpl.java
@@ -1,12 +1,61 @@
 package com.simi.service.impl.xcloud;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.meijia.utils.TimeStampUtil;
+import com.simi.po.dao.xcloud.XcompanyDeptMapper;
+import com.simi.po.dao.xcloud.XcompanyMapper;
+import com.simi.po.model.xcloud.Xcompany;
+import com.simi.po.model.xcloud.XcompanyDept;
 import com.simi.service.xcloud.XcompanyDeptService;
 
 @Service
 public class XcompanyDeptServiceImpl implements XcompanyDeptService {
 	
-		
+	@Autowired
+	XcompanyDeptMapper xCompanyDeptMapper;		
+	
+	
+	@Override
+	public XcompanyDept initXcompanyDept() {
+		XcompanyDept record = new XcompanyDept();
+		record.setDeptId(0L);
+		record.setCompanyId(0L);
+		record.setName("");
+		record.setParentId(0L);
+
+		return record;
+	}
+
+	@Override
+	public int deleteByPrimaryKey(Long id) {
+		return xCompanyDeptMapper.deleteByPrimaryKey(id);
+	}
+	
+	@Override
+	public int insert(XcompanyDept record) {
+		return xCompanyDeptMapper.insert(record);
+	}
+	
+	@Override
+	public int insertSelective(XcompanyDept record) {
+		return xCompanyDeptMapper.insertSelective(record);
+	}
+
+	@Override
+	public XcompanyDept selectByPrimaryKey(Long id) {
+		return xCompanyDeptMapper.selectByPrimaryKey(id);
+	}
+	
+	@Override
+	public int updateByPrimaryKey(XcompanyDept record) {
+		return xCompanyDeptMapper.updateByPrimaryKey(record);
+	}
+
+	@Override
+	public int updateByPrimaryKeySelective(XcompanyDept record) {
+		return xCompanyDeptMapper.updateByPrimaryKeySelective(record);
+	}
 
 }
--- simi-service/src/main/java/com/simi/service/xcloud/XcompanyDeptService.java
+++ simi-service/src/main/java/com/simi/service/xcloud/XcompanyDeptService.java
@@ -1,5 +1,22 @@
 package com.simi.service.xcloud;
 
+import com.simi.po.model.xcloud.Xcompany;
+import com.simi.po.model.xcloud.XcompanyDept;
+
 public interface XcompanyDeptService {
+
+	XcompanyDept initXcompanyDept();
+
+	int deleteByPrimaryKey(Long id);
+
+	int insertSelective(XcompanyDept record);
+
+	XcompanyDept selectByPrimaryKey(Long id);
+
+	int updateByPrimaryKey(XcompanyDept record);
+
+	int updateByPrimaryKeySelective(XcompanyDept record);
+
+	int insert(XcompanyDept record);
 
 }
