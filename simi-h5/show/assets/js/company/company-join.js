02e391ae9cabca78095130fd40b0050d4bcc1725
leon <leon@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-12-02 13:35:49 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@868 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-h5/show/assets/js/company/company-join.js
@@ -0,0 +1,114 @@
+if ($.AMUI && $.AMUI.validator) {
+	$.AMUI.validator.patterns.join_mobile = /^\s*1\d{10}\s*$/;
+}
+
+$('#company-join-form').validator({
+	validate : function(validity) {
+		// Ajax 验证
+		if ($(validity.field).is('.js-ajax-validate')) {
+			// 异步操作必须返回 Deferred 对象
+			var mobile = $('join_mobile').val();
+			if (mobile == undefined || mobile == "")  {
+				validity.valid = false;
+				return validity;
+			}
+			var params = {};
+			params.mobile = $('join_mobile').val();
+			params.sms_token = $('join_sms_token').val();
+			params.sms_type = 3;
+			return $.ajax({
+				type : 'GET',
+				url : appRootUrl + '/user/val_sms_token.json',
+				cache: false,
+				async : false,
+				data : params,
+				dataType : 'json'
+			}).then(function(data) {
+				if (data.status == "999") validity.valid = false;
+				if (data.status == "0") validity.valid = true;
+				return validity;
+			}, function() {
+				return validity;
+			});
+		}
+
+	}
+});
+
+$("#join-submit").on('click', function(e) {
+	console.log("join-submit click");
+	var $form = $('#company-join-form');
+	console.log($form.validator());
+
+	var validator = $form.data('amui.validator');
+
+	if (!validator.isFormValid()) {
+		return false;
+	}
+	var userName =  $('#join_mobile').val();
+	
+	var params = {};
+	params.user_name = userName;
+	params.sms_token = $('#sms_token').val();
+	params.invitation_code = $('#invitation_code').val();
+	//提交数据，完成注册流程
+	$.ajax({
+		type : "POST",
+		url: appRootUrl + "/company/join.json", //发送给服务器的url
+		data : params,
+		dataType : "json",
+		async : false,
+		success : function(data) {
+			
+			if (data.status == "999") {
+				alert(data.msg);
+				return false;
+			}
+			
+			if (data.status == 0) {
+				location.href = "company-reg-ok.html";
+			}
+		},
+		error:function(){
+			return false;
+		}
+	});
+	
+});
+
+
+//获取验证码功能
+$("#join_btn_sms_token").click(function() {
+	console.log("join_btn_sms_token click");
+	var mobile = $('#join_mobile').val();
+	if (mobile == undefined || mobile == "")  {
+		alert("请输入手机号!");
+		return false;
+	}
+	
+	if (!verifyMobile(mobile)) {
+		alert("请输入正确的手机号!");
+		return false;
+	}
+	
+	var params = {};
+	params.mobile = mobile;
+	params.sms_type = 3;
+	//发送验证码
+	$.ajax({
+		type : "GET",
+		url: appRootUrl + "/user/get_sms_token.json", //发送给服务器的url
+		data : params,
+		dataType : "json",
+		async : false,
+		success : function(data) {
+			$("#join_btn_sms_token").html('<span id=\"cd1\">60</span>').attr('disabled','disabled');
+	    	$('#cd1').countDown(function(){
+	    		$("#join_btn_sms_token").html('获取验证码').removeAttr('disabled');
+			})
+		}
+	})
+	
+	
+});
+
