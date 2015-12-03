602db4c97327d3a1567b0a0e30c450f667578f8b
leon <leon@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-12-03 03:55:13 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@872 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-app/src/main/java/com/simi/action/OController.java
@@ -0,0 +1,93 @@
+package com.simi.action;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.meijia.utils.StringUtil;
+import com.simi.action.app.BaseController;
+import com.simi.po.model.user.Users;
+import com.simi.service.user.UserDetailScoreService;
+import com.simi.service.user.UsersService;
+
+@Controller
+@RequestMapping(value = "/app/")
+public class OController extends BaseController {
+
+	@Autowired
+	private UserDetailScoreService userDetailScoreService;
+	@Autowired
+	private UsersService userService;
+	
+	//跳转到对应的h5页面
+	/**
+	 * 
+	 * @param t 即服务类型现有6项，包括：
+				company，公司注册或人员加入
+				meeting，会议或会议室
+				express，快递
+				water，送水
+				clean，保洁
+				green，绿植
+	 * @param userId
+	 * @return
+	 */
+	@SuppressWarnings("rawtypes")
+	@RequestMapping(value = "o", method = RequestMethod.GET)
+	public String redirectUrl(
+			@RequestParam(value = "t", required = false, defaultValue = "") String t,
+			@RequestParam(value = "a", required = false, defaultValue = "") String a,
+			@RequestParam(value = "uid", required = false, defaultValue = "") String uid
+		) {
+		
+		if (StringUtil.isEmpty(t) || StringUtil.isEmpty(a) || StringUtil.isEmpty(uid)) {
+			return "";
+		}
+		
+		Long userId = Long.valueOf(uid);
+		Users users = userService.selectByPrimaryKey(userId);
+		
+		// 判断是否为注册用户，非注册用户返回 999
+		if (users == null) {
+			return "";
+		}		
+		
+		String url = "http://123.57.173.36/simi-h5/show/";
+		
+		switch (t) {
+			case "company" :
+				url = "http://123.57.173.36/simi-h5/show/company-reg.html";
+				url = url + "?uid="+uid;
+				break;
+			case "meeting" :
+				url = "http://123.57.173.36/simi-h5/show/order-meeting.html";
+				url = url + "?uid="+uid;
+				break;
+			case "express" :
+				url = "http://m.kuaidi100.com/";
+				break;
+			case "water" :
+				url = "http://123.57.173.36/simi-h5/show/order-water.html";
+				url = url + "?uid="+uid;
+				break;
+			case "clean" :
+				url = "http://light.yunjiazheng.com/oncecleaning/";
+				break;
+			case "green" :
+				url = "http://123.57.173.36/simi-h5/show/order-green.html";
+				url = url + "?uid="+uid;
+				break;
+			case "checkin" :
+				url = "http://123.57.173.36/simi-h5/show/order-checkin.html";
+				url = url + "?uid="+uid;
+				break;
+		}
+
+		return "redirect:"+url;
+		
+	}
+	
+
+}
