281e0a8c9cb749dda56555d44aeab9ca195e72fe
aimee <aimee@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-12-12 05:16:22 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@943 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-service/src/main/java/com/simi/service/partners/PartnerRefServiceTypeService.java
@@ -0,0 +1,23 @@
+package com.simi.service.partners;
+
+import com.simi.po.model.partners.PartnerRefServiceType;
+
+public interface PartnerRefServiceTypeService {
+	
+	int deleteByPrimaryKey(Long id);
+
+    int insert(PartnerRefServiceType record);
+
+    int insertSelective(PartnerRefServiceType record);
+
+    PartnerRefServiceType selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(PartnerRefServiceType record);
+
+    int updateByPrimaryKey(PartnerRefServiceType record);
+    
+    PartnerRefServiceType initPartnerRefServiceType();
+
+	int deleteByPartnerId(Long partnerId);
+
+}
