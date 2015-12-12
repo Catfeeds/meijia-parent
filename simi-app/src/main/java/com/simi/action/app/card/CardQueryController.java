b15d8bfc20290158b5dce4dbc00686456ed88af4
leon <leon@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-12-12 09:38:47 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@954 52ac4245-7803-4e87-b018-04b7c5ce731e

--- simi-app/src/main/java/com/simi/action/app/card/CardQueryController.java
+++ simi-app/src/main/java/com/simi/action/app/card/CardQueryController.java
@@ -340,56 +340,5 @@ public class CardQueryController extends BaseController {
 		result.setData(remindCards);
 		
 		return result;
-	}
-	
-	@RequestMapping(value = "push_test", method = RequestMethod.GET)	
-	public AppResultData<Object> pushTest(
-			@RequestParam("card_id") Long cardId,
-			@RequestParam(value = "clinet_id", required = false, defaultValue = "b10510a6a8d000fb024af47271f8a49f") String clientId,
-			
-			@RequestParam(value = "device_type", required = false, defaultValue = "ios") String deviceType
-			) throws Exception {
-		
-		AppResultData<Object> result = new AppResultData<Object>(Constants.SUCCESS_0, ConstantMsg.SUCCESS_0_MSG, "");
-		
-		HashMap<String, String> params = new HashMap<String, String>();
-		params.put("cid", clientId);
-		
-		HashMap<String, String> tranParams = new HashMap<String, String>();
-		 
-
-		
-		 Long time1 = TimeStampUtil.getNow() + 120000;
-		 
-		 String timeStr1 = TimeStampUtil.timeStampToDateStr(time1, "MM-dd HH:mm");
-		 
-		 String timeStr = time1.toString();
-		 tranParams.put("is_show", "true");		 
-		 tranParams.put("card_id", cardId.toString());
-		 tranParams.put("card_type", "1");
-		 tranParams.put("service_time", timeStr);
-		 tranParams.put("remind_time", timeStr);
-		 tranParams.put("remind_title", "会议安排");
-		 tranParams.put("remind_content", timeStr1 + "请参加华北区电话会议,请提前10分钟进入会议室B11");
-
-//		 JsonObject jsonParams = JsonUtil.mapTojson(tranParams);
-		 
-		 ObjectMapper objectMapper = new ObjectMapper();
-		 String jsonParams = objectMapper.writeValueAsString(tranParams);
-		 System.out.println(jsonParams);
-		 
-		params.put("transmissionContent", jsonParams);
-
-		if (deviceType.equals("ios")) {
-			PushUtil.IOSPushToSingle(params);
-		}
-		
-		if (deviceType.equals("android")) {
-			PushUtil.AndroidPushToSingle(params);
-		}
-		
-		
-		return result;
 	}	
-	
 }
