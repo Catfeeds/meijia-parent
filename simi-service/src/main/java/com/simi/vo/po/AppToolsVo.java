d8f111a4c3d1f0fbb0853f1e67d081a3502e1d50
aimee <aimee@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-12-22 04:32:33 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@1010 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-service/src/main/java/com/simi/vo/po/AppToolsVo.java
@@ -0,0 +1,18 @@
+package com.simi.vo.po;
+
+import com.simi.po.model.op.AppTools;
+
+public class AppToolsVo extends AppTools {
+
+	//添加时间戳
+	private String addTimeStr ;
+
+	public String getAddTimeStr() {
+		return addTimeStr;
+	}
+
+	public void setAddTimeStr(String addTimeStr) {
+		this.addTimeStr = addTimeStr;
+	}
+
+}
