6fdfcade21e8ac7a8a94c5ab7b6c70929f65704b
aimee <aimee@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-12-23 09:38:39 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@1031 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-service/src/main/java/com/simi/vo/partners/PartnerServicePriceDetailVoAll.java
@@ -0,0 +1,43 @@
+package com.simi.vo.partners;
+
+
+import com.simi.po.model.partners.PartnerServicePriceDetail;
+
+
+public class PartnerServicePriceDetailVoAll extends PartnerServicePriceDetail {
+	
+	private String name;
+	
+	private Long parentId;
+	
+	private Long partnerId;
+	
+	private Integer no;
+		
+	public Long getParentId() {
+		return parentId;
+	}
+	public void setParentId(Long parentId) {
+		this.parentId = parentId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getNo() {
+		return no;
+	}
+	public void setNo(Integer no) {
+		this.no = no;
+	}
+	public Long getPartnerId() {
+		return partnerId;
+	}
+	public void setPartnerId(Long partnerId) {
+		this.partnerId = partnerId;
+	}
+
+
+}
