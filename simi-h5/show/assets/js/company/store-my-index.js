abb3b3f7086a770486e47dfbd8a033256ec548d1
aimee <aimee@52ac4245-7803-4e87-b018-04b7c5ce731e>
2015-12-26 11:59:34 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@1075 52ac4245-7803-4e87-b018-04b7c5ce731e

--- simi-h5/show/assets/js/company/store-my-index.js
+++ simi-h5/show/assets/js/company/store-my-index.js
@@ -4,6 +4,6 @@ var userId = getUrlParam("user_id");
 //商品管理
 $("#storePrice").attr("href","store-price-list.html?user_id="+userId);
 //订单处理
-$("#orderControll").attr("href","store-order-list.html?partner_user_id="+userId);
+$("#orderControll").attr("href","store-order-list.html?user_id="+userId);
 //人员管理
 $("#userControll").attr("href","invite-index.html");
