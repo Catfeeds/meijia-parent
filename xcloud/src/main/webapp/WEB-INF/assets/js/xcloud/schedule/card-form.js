2ea3269e6a5687fbcbc72fd623a2390556a45b6d
leon <leon@52ac4245-7803-4e87-b018-04b7c5ce731e>
2016-01-12 14:00:47 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@1175 52ac4245-7803-4e87-b018-04b7c5ce731e

--- xcloud/src/main/webapp/WEB-INF/assets/js/xcloud/schedule/card-form.js
+++ xcloud/src/main/webapp/WEB-INF/assets/js/xcloud/schedule/card-form.js
@@ -1,4 +1,3 @@
-
 //初始化员工表格
 $("#list-table").DataTable(
 		{
@@ -34,10 +33,8 @@ $("#list-table").DataTable(
 					{
 						"data" : "user_id",
 						"render" : function(data, type, full, meta) {
-							return '<input type="checkbox" onclick="setSelectTable('
-									+ data
-									+ ')" id="user_id" name="user_id" value="'
-									+ data + '"\>';
+							return '<input type="checkbox" onclick="setSelectTable(' + data
+									+ ')" id="user_id" name="user_id" value="' + data + '"\>';
 						}
 					}, {
 						"data" : "job_number"
@@ -56,22 +53,22 @@ $("#list-table").DataTable(
 			]
 		});
 
-//初始化选择后的效果
+// 初始化选择后的效果
 $('#selected_users').tagsinput({
-	  itemValue: 'id',
-	  itemText: 'label',
-	  allowDuplicates: false,
-	  trimValue: true,
-	  maxChars: 8
+	itemValue : 'id',
+	itemText : 'label',
+	allowDuplicates : false,
+	trimValue : true,
+	maxChars : 8
 });
 
-//表格中checkbox 选择触发事件
+// 表格中checkbox 选择触发事件
 function setSelectTable(userId) {
 	var table = $("#list-table").DataTable();
 	console.log("userId = " + userId);
 
 	var name = "";
-	
+
 	table.$('input[type="checkbox"]').each(function() {
 
 		var $row = this.closest('tr');
@@ -84,29 +81,34 @@ function setSelectTable(userId) {
 			if (this.checked) {
 				console.log("add");
 				addSelectUser(userId, name);
-				$('#selected_users').tagsinput('add', {id:userId, label:name});
+				$('#selected_users').tagsinput('add', {
+					id : userId,
+					label : name
+				});
 			} else {
 				console.log("remove");
 				removeSelectUser(userId, name);
-				$('#selected_users').tagsinput('remove', {id:userId, label:name});
+				$('#selected_users').tagsinput('remove', {
+					id : userId,
+					label : name
+				});
 			}
-			
-			return false;
-		}		
-	});
 
+			return false;
+		}
+	});
 
 	$('#selected_users').tagsinput('refresh');
 
 }
 
-//表格中 checkBox 未选中的事件处理
+// 表格中 checkBox 未选中的事件处理
 function removeSelectTable(userId) {
 	var table = $("#list-table").DataTable();
 	console.log("userId = " + userId);
 
 	var name = "";
-	
+
 	table.$('input[type="checkbox"]').each(function() {
 
 		var $row = this.closest('tr');
@@ -114,35 +116,34 @@ function removeSelectTable(userId) {
 
 		if (data.user_id == userId) {
 			this.checked = false;
-			
+
 			return false;
-		}		
+		}
 	});
 
-
-//	$('#selected_users').tagsinput('refresh');
+	// $('#selected_users').tagsinput('refresh');
 
 }
 
-//hidden selectUserIds 和 selectUserNames 处理添加的情况
+// hidden selectUserIds 和 selectUserNames 处理添加的情况
 function addSelectUser(userId, name) {
 	console.log("addSelectUser");
 	var selectUserIds = $("#selectUserIds").val();
 	var selectUserNames = $("#selectUserNames").html();
-	console.log("selectUserIds = " +selectUserIds);
-	console.log("selectUserNames = " +selectUserNames);
+	console.log("selectUserIds = " + selectUserIds);
+	console.log("selectUserNames = " + selectUserNames);
 	var hasExist = false;
 	if (selectUserIds != "") {
 		var selectUserAry = selectUserIds.split(',');
 		for (var i = 0; i < selectUserAry.length; i++) {
 			console.log(selectUserAry[i] + "---------" + userId);
-	        if (selectUserAry[i] == userId) {
-	        	hasExist = true;
-	            break;
-	        }
-	    }
+			if (selectUserAry[i] == userId) {
+				hasExist = true;
+				break;
+			}
+		}
 	}
-	
+
 	if (hasExist == false) {
 		selectUserIds = selectUserIds + userId + ",";
 		selectUserNames = selectUserNames + name + ",";
@@ -153,14 +154,14 @@ function addSelectUser(userId, name) {
 	console.log($("#selectUserNames").html());
 };
 
-//hidden selectUserIds 和 selectUserNames 处理移除的情况
+// hidden selectUserIds 和 selectUserNames 处理移除的情况
 function removeSelectUser(userId, name) {
 	var selectUserIds = $("#selectUserIds").val();
 	var selectUserNames = $("#selectUserNames").html();
-	
-	console.log("selectUserIds = " +selectUserIds);
-	console.log("selectUserNames = " +selectUserNames);
-	
+
+	console.log("selectUserIds = " + selectUserIds);
+	console.log("selectUserNames = " + selectUserNames);
+
 	var hasExist = false;
 	var newSelectUserIds = "";
 	var newSelectNames = "";
@@ -168,57 +169,64 @@ function removeSelectUser(userId, name) {
 		var selectUserAry = selectUserIds.split(',');
 		var selectNameAry = selectUserNames.split(',');
 		for (var i = 0; i < selectUserAry.length; i++) {
-	        if (selectUserAry[i] != userId && selectUserAry[i] != "") {
-	        	newSelectUserIds+= selectUserAry[i] + ",";
-	        	newSelectNames+= selectNameAry[i] + ",";
-	        }
-	    }
+			if (selectUserAry[i] != userId && selectUserAry[i] != "") {
+				newSelectUserIds += selectUserAry[i] + ",";
+				newSelectNames += selectNameAry[i] + ",";
+			}
+		}
 	}
-	
+
 	$("#selectUserIds").val(newSelectUserIds);
 	$("#selectUserNames").html(newSelectNames);
-	
+
 	console.log($("#selectUserIds").val());
 };
 
-//tagsInput 删除元素完成后的触发事件
+// tagsInput 删除元素完成后的触发事件
 $('#selected_users').on('itemRemoved', function(event) {
-	  // event.item: contains the item
+	// event.item: contains the item
 	console.log("itemRemoved");
 	var item = event.item;
 	var userId = item.id;
 	var name = item.label;
 	console.log("userId = " + userId);
 	removeSelectUser(userId, name);
-	
+
 	removeSelectTable(userId);
-	
+
 });
 
-//console.log(moment().add('hours',1).format('YYYY-MM-DD HH:00'));
-var curFormDateTime = moment().add('m',5).format('YYYY-MM-DD HH:mm');
+// console.log(moment().add('hours',1).format('YYYY-MM-DD HH:00'));
+var curFormDateTime = moment().add('m', 5).format('YYYY-MM-DD HH:mm');
 $('.form_datetime').datetimepicker({
-    format: 'yyyy-mm-dd hh:ii',
-    autoclose: true,
-    todayBtn: true,
-    startDate: curFormDateTime,
-    minuteStep: 5
+	format : 'yyyy-mm-dd hh:ii',
+	autoclose : true,
+	todayBtn : true,
+	startDate : curFormDateTime,
+	minuteStep : 1
 });
 
 $('#setNowSend').not('[data-switch-no-init]').bootstrapSwitch();
 
+// 表单验证设置
+// $('#card-form').validator({
+// allFields: ':input',
+// });
 
-//表单验证设置
-//$('#card-form').validator({
-//	allFields: ':input',
-//});
-
-
-//提交验证
+// 提交验证
 $("#btn-card-submit").on('click', function(e) {
+
+	var fv = formValidation();
+	console.log("fv  =" + fv);
 	
+	if (fv == false) return false;
+	
+	
+});
+
+function formValidation() {
 	var selectUserIds = $("#selectUserIds").val();
-	
+
 	var selectFlag = true;
 	if (selectUserIds == "") {
 		selectFlag = false;
@@ -226,15 +234,24 @@ $("#btn-card-submit").on('click', function(e) {
 	}
 	
 	var form = $('#card-form');
-	
+
 	var formValidity = $('#card-form').validator().data('amui.validator').validateForm().valid
-	
+
 	if (formValidity && selectFlag) {
 		// done, submit form
 		console.log("ok");
-//		form.submit();
-	} else  {
+		//判断提醒时间点可以提醒.
+		//1. 获取提醒时间
+		
+		//2. 获取提醒设置
+		
+		
+		return true;
+		// form.submit();
+	} else {
 		// fail
 		console.log("fail");
-	};
-});
+		return false;
+	}
+	;
+}
