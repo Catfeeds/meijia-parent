dc7e249fdc7ea2037a262cd0d6810630414c0c37
leon <leon@52ac4245-7803-4e87-b018-04b7c5ce731e>
2016-01-26 08:49:33 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@1215 52ac4245-7803-4e87-b018-04b7c5ce731e

--- simi-service/src/main/java/com/simi/service/impl/async/UserMsgAsyncServiceImpl.java
+++ simi-service/src/main/java/com/simi/service/impl/async/UserMsgAsyncServiceImpl.java
@@ -158,7 +158,7 @@ public class UserMsgAsyncServiceImpl implements UserMsgAsyncService {
 		return new AsyncResult<Boolean>(true);
 	}		
 	
-	@Async
+//	@Async
 	@Override
 	public Future<Boolean> newImMsg(Long 	fromUserId, 
 									String 	fromUserName, 
