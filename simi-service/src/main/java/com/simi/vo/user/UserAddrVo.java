6c071fece85dff2b988b4f451c6c51336dfe826f
aimee <aimee@52ac4245-7803-4e87-b018-04b7c5ce731e>
2016-02-29 11:19:44 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@1313 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-service/src/main/java/com/simi/vo/user/UserAddrVo.java
@@ -0,0 +1,26 @@
+package com.simi.vo.user;
+
+public class UserAddrVo  {
+
+	private String addrName;
+	
+    private Long addrId;
+
+	public String getAddrName() {
+		return addrName;
+	}
+
+	public void setAddrName(String addrName) {
+		this.addrName = addrName;
+	}
+
+	public Long getAddrId() {
+		return addrId;
+	}
+
+	public void setAddrId(Long addrId) {
+		this.addrId = addrId;
+	}
+
+   
+}
