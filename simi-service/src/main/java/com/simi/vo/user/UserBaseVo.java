25aa1253596a749e7dc59dd6bbd097356410bc7b
leon <leon@52ac4245-7803-4e87-b018-04b7c5ce731e>
2016-03-01 02:40:01 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@1316 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-service/src/main/java/com/simi/vo/user/UserBaseVo.java
@@ -0,0 +1,65 @@
+package com.simi.vo.user;
+
+public class UserBaseVo  {
+	
+	private long userId;
+	
+	private String mobile;
+	
+	private	String headImg;
+	
+	private String name;
+	
+	private String sex;
+	
+	private String provinceName;
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getHeadImg() {
+		return headImg;
+	}
+
+	public void setHeadImg(String headImg) {
+		this.headImg = headImg;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getProvinceName() {
+		return provinceName;
+	}
+
+	public void setProvinceName(String provinceName) {
+		this.provinceName = provinceName;
+	}
+	
+}
