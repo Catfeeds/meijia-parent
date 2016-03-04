7803deeb46b91a27d08454d771178dfb58404a60
leon <leon@52ac4245-7803-4e87-b018-04b7c5ce731e>
2016-03-04 10:27:11 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@1383 52ac4245-7803-4e87-b018-04b7c5ce731e

--- simi-app/src/main/java/com/simi/action/app/user/UserCardController.java
+++ simi-app/src/main/java/com/simi/action/app/user/UserCardController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.simi.action.app.BaseController;
 import com.simi.common.ConstantMsg;
 import com.simi.common.Constants;
+import com.simi.po.model.dict.DictCardType;
+import com.simi.po.model.order.OrderCards;
+import com.simi.po.model.user.Users;
 import com.simi.service.dict.CardTypeService;
 import com.simi.service.order.OrderCardsService;
 import com.simi.service.user.UsersService;
@@ -47,40 +50,40 @@ public class UserCardController extends BaseController {
 		AppResultData<Object> result = new AppResultData<Object>( Constants.SUCCESS_0, ConstantMsg.SUCCESS_0_MSG, "");		
 		
 		//暂时不能进行充值功能：
-		result.setStatus(Constants.ERROR_999);
-		result.setMsg("系统维护,暂时无法充值.");
-		return result;
-		
-		
-//		Users users = usersService.selectByPrimaryKey(userId);
-//		
-//		// 判断是否为注册用户，非注册用户返回 999
-//		if (users == null) {
-//			result.setStatus(Constants.ERROR_999);
-//			result.setMsg(ConstantMsg.USER_NOT_EXIST_MG);
-//			return result;
-//		}		
-//		
-//		if (cardType.equals(0L)) {
-//			if (cardMoney == null || cardMoney.equals(0)) {
-//				result.setStatus(Constants.ERROR_999);
-//				result.setMsg("充值金额不正确!");
-//				return result;
-//			}
-//			
-//			OrderCards record = orderCardsService.initCardMoney(users, cardType, cardMoney, payType);
-//			orderCardsService.insert(record);
-//			result.setData(record);
-//			return result;
-//		}
-//		
-//		
-//		DictCardType dictCardType = cardTypeService.selectByPrimaryKey(cardType);
-//
-//		OrderCards record = orderCardsService.initOrderCards(users, cardType, dictCardType, payType);
-//		orderCardsService.insert(record);
-//
-//		result.setData(record);
+//		result.setStatus(Constants.ERROR_999);
+//		result.setMsg("系统维护,暂时无法充值.");
 //		return result;
+		
+		
+		Users users = usersService.selectByPrimaryKey(userId);
+		
+		// 判断是否为注册用户，非注册用户返回 999
+		if (users == null) {
+			result.setStatus(Constants.ERROR_999);
+			result.setMsg(ConstantMsg.USER_NOT_EXIST_MG);
+			return result;
+		}		
+		
+		if (cardType.equals(0L)) {
+			if (cardMoney == null || cardMoney.equals(0)) {
+				result.setStatus(Constants.ERROR_999);
+				result.setMsg("充值金额不正确!");
+				return result;
+			}
+			
+			OrderCards record = orderCardsService.initCardMoney(users, cardType, cardMoney, payType);
+			orderCardsService.insert(record);
+			result.setData(record);
+			return result;
+		}
+		
+		
+		DictCardType dictCardType = cardTypeService.selectByPrimaryKey(cardType);
+
+		OrderCards record = orderCardsService.initOrderCards(users, cardType, dictCardType, payType);
+		orderCardsService.insert(record);
+
+		result.setData(record);
+		return result;
 	}
 }
