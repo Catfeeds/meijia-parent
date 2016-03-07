124c94d2f068235eaa9e9a9362ef154371f01d77
leon <leon@52ac4245-7803-4e87-b018-04b7c5ce731e>
2016-03-07 07:42:28 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@1405 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ update-simi-all.sh
@@ -0,0 +1,16 @@
+cd /data/src/meijia-parent/
+svn up
+mvn clean package
+/data/tomcat/bin/shutdown.sh
+sleep 1
+rm -rf /data/tomcat/webapps/simi
+\cp -rf /data/src/meijia-parent/simi-app/target/simi.war /data/tomcat/webapps/
+
+rm -rf /data/tomcat/webapps/simi-oa
+\cp -rf /data/src/meijia-parent/simi-oa/target/simi-oa.war /data/tomcat/webapps/
+
+rm -rf /data/tomcat/webapps/xcloud
+\cp -rf /data/src/meijia-parent/xcloud/target/xcloud.war /data/tomcat/webapps/
+
+/data/tomcat/bin/startup.sh
+
