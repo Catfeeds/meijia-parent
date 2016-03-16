a618e8e160777afd95f529bfec5657d7c7adead2
aimee <aimee@52ac4245-7803-4e87-b018-04b7c5ce731e>
2016-03-16 10:19:40 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@1481 52ac4245-7803-4e87-b018-04b7c5ce731e

--- xcloud/src/main/webapp/WEB-INF/assets/js/xcloud/staffs/personal-form.js
+++ xcloud/src/main/webapp/WEB-INF/assets/js/xcloud/staffs/personal-form.js
@@ -36,31 +36,36 @@ $("#btn-personal-submit").on('click', function(e) {
 				}
 
 				if (data.status == 0) {
-					location.href = "/xcloud/index";
-				}
-			}
-		})
-		var params = {}
-		params.user_id = $("#userId").val();
-		$.ajax({
-			type : "POST",
-			url : appRootUrl + "user/post_user_head_img.json", // 发送给服务器的url
-			data : params,
-			secureuri: false, //一般设置为false
-			fileElementId: 'file', // 上传文件的id、name属性名
-			dataType : "json",
-			async : false,
-			success : function(data) {
-				if (data.status == "999") {
-					alert(data.msg);
-					return false;
-				}
+					//location.href = "/xcloud/index";
+					var params = {}
+					params.user_id = $("#userId").val();
+					alert("111111111111");
+					$.ajax({
+						type : "POST",
+						url : appRootUrl + "user/post_user_head_img.json", // 发送给服务器的url
+						data : params,
+						secureuri: false, //一般设置为false
+						fileElementId: 'file', // 上传文件的id、name属性名
+						dataType : "json",
+						async : false,
+						success : function(data) {
+							if (data.status == "999") {
+								alert(data.msg);
+								return false;
+							}
 
-				if (data.status == 0) {
-					location.href = "/xcloud/index";
+							if (data.status == 0) {
+								location.href = "/xcloud/index";
+							}
+						}
+					})
+					
+					
+					//////////////////////////////////////////////
 				}
 			}
 		})
+		
 	} else  {
 		// fail
 		console.log("fail");
