8106c6b81bce12cc202b791c77d302bb152e9d51
aimee <aimee@52ac4245-7803-4e87-b018-04b7c5ce731e>
2016-03-18 03:30:22 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@1499 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-dao/src/main/java/com/simi/vo/xcloud/CompanySearchVo.java
@@ -0,0 +1,18 @@
+package com.simi.vo.xcloud;
+
+
+public class CompanySearchVo{
+
+	
+	private String companyName;
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	
+	
+}
