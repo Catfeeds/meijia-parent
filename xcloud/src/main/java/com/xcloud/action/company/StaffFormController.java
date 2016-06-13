ed0c9879421a5cfeddcbe065ea46824af866ddbc
leon <leon@52ac4245-7803-4e87-b018-04b7c5ce731e>
2016-06-13 12:24:35 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@1856 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ xcloud/src/main/java/com/xcloud/action/company/StaffFormController.java
@@ -0,0 +1,12 @@
+package com.xcloud.action.company;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.xcloud.action.BaseController;
+
+@Controller
+@RequestMapping(value = "/")
+public class StaffFormController extends BaseController {
+
+}
