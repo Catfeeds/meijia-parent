aec999388597eca2203b0ca558718c90c91db13d
leon <leon@52ac4245-7803-4e87-b018-04b7c5ce731e>
2016-06-13 11:47:57 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@1854 52ac4245-7803-4e87-b018-04b7c5ce731e

---
+++ simi-app/src/test/java/com/simi/action/app/job/TestJobHolidayController.java
@@ -0,0 +1,31 @@
+package com.simi.action.app.job;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import com.simi.action.app.JUnitActionBase;
+
+public class TestJobHolidayController extends JUnitActionBase{
+	
+	@Test
+    public void testGetHoliday() throws Exception {
+
+		String url = "/app/job/holiday/get_holiday.json";
+
+     	MockHttpServletRequestBuilder postRequest = get(url);
+
+	    ResultActions resultActions = mockMvc.perform(postRequest);
+
+	    resultActions.andExpect(content().contentType(this.mediaType));
+	    resultActions.andExpect(status().isOk());
+
+
+	    System.out.println("RestultActions: " + resultActions.andReturn().getResponse().getContentAsString());
+
+    }	
+	
+}
