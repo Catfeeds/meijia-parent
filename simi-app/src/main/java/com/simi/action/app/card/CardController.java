82f573ad021e1b93e6b6f4bae56546ce6c593081
leon <leon@52ac4245-7803-4e87-b018-04b7c5ce731e>
2016-06-18 07:55:59 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@1917 52ac4245-7803-4e87-b018-04b7c5ce731e

--- simi-app/src/main/java/com/simi/action/app/card/CardController.java
+++ simi-app/src/main/java/com/simi/action/app/card/CardController.java
@@ -221,8 +221,9 @@ public class CardController extends BaseController {
 		}
 		
 		//如果是立即给相关人员发送消息，则需要短信模板的通知.
-		cardAsyncService.cardNotification(record);
-
+		if (!setRemind.equals((short)0)) {
+			cardAsyncService.cardNotification(record);
+		}
 		//如果是秘书处理，则需要给相应的秘书发送消息.
 		if (record.getSetSecDo().equals((short)1)) {
 			cardAsyncService.cardSecDo(userId, record);
@@ -367,6 +368,12 @@ public class CardController extends BaseController {
 		record.setUpdateTime(TimeStampUtil.getNowSecond());
 		cardService.updateByPrimaryKeySelective(record);
 		
+		//添加卡片log
+		//添加卡片日志
+		cardAsyncService.cardLog(userId, cardId, CardUtil.getStatusName(status));
+		
+		if (record.getSetRemind().equals((short)0)) return result;
+		
 		//需要删除多人的日程信息.
 		if (record.getPeriod().equals((short)0)) {
 			UserMsgSearchVo searchVo = new UserMsgSearchVo();
@@ -394,9 +401,7 @@ public class CardController extends BaseController {
 			}
 		}
 		
-		//添加卡片log
-		//添加卡片日志
-		cardAsyncService.cardLog(userId, cardId, CardUtil.getStatusName(status));
+		
 
 		return result;
 	}	
