7ec65d2938728e3b36ed265c3a4e1832e71253ef
leon <leon@52ac4245-7803-4e87-b018-04b7c5ce731e>
2016-06-21 06:26:12 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@1933 52ac4245-7803-4e87-b018-04b7c5ce731e

--- meijia-utils/src/main/java/com/meijia/utils/GsonUtil.java
+++ meijia-utils/src/main/java/com/meijia/utils/GsonUtil.java
@@ -79,7 +79,16 @@ public class GsonUtil {
 //		return list;
 
 		List<T> list = new ArrayList<T>();
-		JsonArray array = new JsonParser().parse(gsonString).getAsJsonArray();
+		
+		String utf8 = "";
+		try {
+			utf8 = new String(gsonString.getBytes("iso-8859-1"), "utf-8");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		JsonArray array = new JsonParser().parse(utf8).getAsJsonArray();
 		for (final JsonElement elem : array) {
 			list.add(new Gson().fromJson(elem, cls));
 		}
