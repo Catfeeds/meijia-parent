c375224940ad8a00f689e66ae0b1765312d6ef1d
leon <leon@52ac4245-7803-4e87-b018-04b7c5ce731e>
2016-08-23 08:43:59 +0000

    git-svn-id: svn://123.57.173.36/meijia-parent@2100 52ac4245-7803-4e87-b018-04b7c5ce731e

--- xcloud/src/main/java/com/xcloud/action/LoginController.java
+++ xcloud/src/main/java/com/xcloud/action/LoginController.java
@@ -31,116 +31,140 @@ import com.simi.service.xcloud.XcompanyStaffService;
 import com.simi.vo.xcloud.CompanyAdminSearchVo;
 import com.simi.vo.xcloud.UserCompanySearchVo;
 
-
 @Controller
 @RequestMapping(value = "/")
 public class LoginController extends BaseController {
-	
+
 	@Autowired
-	private XCompanyService xCompanyService;	
-	
+	private XCompanyService xCompanyService;
+
 	@Autowired
-	private XcompanyAdminService xCompanyAdminService;	
-	
+	private XcompanyAdminService xCompanyAdminService;
+
 	@Autowired
 	private XcompanyStaffService xcompanyStaffService;
-	
-	@Autowired
-	private UsersService usersService;		
 
-	@RequestMapping(value="/login", method = {RequestMethod.GET})
-    public String login(Model model){
-		if(!model.containsAttribute("contentModel")) {
+	@Autowired
+	private UsersService usersService;
+
+	@RequestMapping(value = "/login", method = { RequestMethod.GET })
+	public String login(Model model) {
+		if (!model.containsAttribute("contentModel")) {
 			LoginVo vo = new LoginVo();
-//			vo.setUsername("13810002890");
-//			vo.setPassword("000000");
+			// vo.setUsername("13810002890");
+			// vo.setPassword("000000");
 			model.addAttribute("contentModel", vo);
 		}
-            
-        return "/home/login";
-    }
 
-	@RequestMapping(value="/login", method = {RequestMethod.POST})
-	public String login(HttpServletRequest request, Model model, 
-			@Valid @ModelAttribute("contentModel") LoginVo loginVo ,
-			BindingResult result) throws NoSuchAlgorithmException{
-		//如果有验证错误 返回到form页面
-        if (result.hasErrors())
-            return login(model);
+		return "/home/login";
+	}
 
-        String userName = loginVo.getUsername().trim();
-        String password = loginVo.getPassword().trim();
-        
-        String passwordMd5 = StringUtil.md5(password.trim());
-        
-        CompanyAdminSearchVo searchVo1 = new CompanyAdminSearchVo();
+	@RequestMapping(value = "/login", method = { RequestMethod.POST })
+	public String login(HttpServletRequest request, Model model, @Valid @ModelAttribute("contentModel") LoginVo loginVo, BindingResult result)
+			throws NoSuchAlgorithmException {
+		// 如果有验证错误 返回到form页面
+		if (result.hasErrors())
+			return login(model);
+
+		String userName = loginVo.getUsername().trim();
+		String password = loginVo.getPassword().trim();
+
+		String passwordMd5 = StringUtil.md5(password.trim());
+
+		CompanyAdminSearchVo searchVo1 = new CompanyAdminSearchVo();
 		searchVo1.setUserName(userName);
 		searchVo1.setPassMd5(passwordMd5);
-        
-        
-        XcompanyAdmin xCompanyAdmn = null;
-        List<XcompanyAdmin> rs = xCompanyAdminService.selectBySearchVo(searchVo1);
-        
-        if (rs.isEmpty()) {
-        	result.addError(new FieldError("contentModel","username","用户名或密码错误。"));
-        	return login(model);
-        } else {
-        	xCompanyAdmn = rs.get(0);
-        }
-        
-        if ( xCompanyAdmn == null ) {
-        	result.addError(new FieldError("contentModel","username","用户名或密码错误。"));
-        	return login(model);
-    	}
-        Long companyId = xCompanyAdmn.getCompanyId();
-        Xcompany xcompany = xCompanyService.selectByPrimaryKey(companyId);
-        
-        Users u = usersService.selectByMobile(userName);
-        
-		
+
+		XcompanyAdmin xCompanyAdmn = null;
+		List<XcompanyAdmin> rs = xCompanyAdminService.selectBySearchVo(searchVo1);
+
+		Long companyId = 0L;
+		Xcompany xcompany = null;
+		if (rs.isEmpty()) {
+			result.addError(new FieldError("contentModel", "username", "用户名或密码错误。"));
+			return login(model);
+		} else {
+			if (rs.size() == 1) {
+				xCompanyAdmn = rs.get(0);
+			} else {
+				Users u = usersService.selectByMobile(userName);
+				UserCompanySearchVo searchVo = new UserCompanySearchVo();
+				searchVo.setUserId(u.getId());
+				// searchVo.setIsDefault((short) 1);
+				List<XcompanyStaff> list = xcompanyStaffService.selectBySearchVo(searchVo);
+
+				for (XcompanyStaff item : list) {
+					if (item.getIsDefault().equals((short) 1)) {
+						companyId = item.getCompanyId();
+						break;
+					}
+				}
+
+				if (companyId.equals(0L)) {
+					xCompanyAdmn = rs.get(0);
+				} else {
+					for (XcompanyAdmin item : rs) {
+						if (item.getCompanyId().equals(companyId)) {
+							xCompanyAdmn = item;
+							break;
+						}
+					}
+				}
+
+			}
+		}
+
+		if (xCompanyAdmn == null) {
+			result.addError(new FieldError("contentModel", "username", "用户名或密码错误。"));
+			return login(model);
+		}
+
+		companyId = xCompanyAdmn.getCompanyId();
+		xcompany = xCompanyService.selectByPrimaryKey(companyId);
+
+		Users u = usersService.selectByMobile(userName);
+
 		Long userId = u.getId();
 		String companyName = xcompany.getCompanyName();
 		String shortName = xcompany.getShortName();
 		String headImg = usersService.getHeadImg(u);
-		
-		//员工信息
+
+		// 员工信息
 		UserCompanySearchVo searchVo = new UserCompanySearchVo();
 		searchVo.setCompanyId(companyId);
 		searchVo.setUserId(userId);
 		searchVo.setStatus((short) 1);
-		
+
 		List<XcompanyStaff> rsList = xcompanyStaffService.selectBySearchVo(searchVo);
 		XcompanyStaff xcompanyStaffExist = null;
 		if (!rsList.isEmpty()) {
 			xcompanyStaffExist = rsList.get(0);
 		}
 
+		AccountAuth accountAuth = new AccountAuth();
+		accountAuth.setUserId(userId);
+		accountAuth.setName(u.getName());
+		accountAuth.setHeadImg(headImg);
+		accountAuth.setRealName(u.getRealName());
+		accountAuth.setMobile(u.getMobile());
+		accountAuth.setCompanyId(companyId);
+		accountAuth.setCompanyName(companyName);
+		accountAuth.setShortName(shortName);
 
-        AccountAuth accountAuth= new AccountAuth();
-        accountAuth.setUserId(userId);
-        accountAuth.setName(u.getName());
-        accountAuth.setHeadImg(headImg);
-        accountAuth.setRealName(u.getRealName());
-        accountAuth.setMobile(u.getMobile());
-        accountAuth.setCompanyId(companyId);
-        accountAuth.setCompanyName(companyName);
-        accountAuth.setShortName(shortName);
-        
-        if (xcompanyStaffExist != null) {
-        	accountAuth.setStaffId(xcompanyStaffExist.getId());
-        }
+		if (xcompanyStaffExist != null) {
+			accountAuth.setStaffId(xcompanyStaffExist.getId());
+		}
 
-    	AuthHelper.setSessionAccountAuth(request, accountAuth);
+		AuthHelper.setSessionAccountAuth(request, accountAuth);
 
-
-        String returnUrl = ServletRequestUtils.getStringParameter(request, "returnUrl", null);
-        if(returnUrl==null)
-        	returnUrl="/index";
-    	return "redirect:"+returnUrl;
+		String returnUrl = ServletRequestUtils.getStringParameter(request, "returnUrl", null);
+		if (returnUrl == null)
+			returnUrl = "/index";
+		return "redirect:" + returnUrl;
 
 	}
 
-	@RequestMapping(value="/logout", method = {RequestMethod.GET})
+	@RequestMapping(value = "/logout", method = { RequestMethod.GET })
 	public String logout(HttpServletRequest request) {
 		AuthHelper.removeSessionAccountAuth(request, "accountAuth");
 		return "redirect:/login";
