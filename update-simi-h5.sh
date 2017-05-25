34476843695fb865e4e2b25ee14a6d1aea35484b
lnczx <lnczx0915@gmail.com>
2017-05-25 19:31:22 +0800

    -- 更新脚本

--- update-simi-h5.sh
+++ update-simi-h5.sh
@@ -1,5 +1,6 @@
 cd /data/src/meijia-parent/
+git reset --hard HEAD
 git pull
-cp -rf /data/src/meijia-parent/simi-h5/show /data/tomcat/webapps/simi-h5
+\cp -rf /data/src/meijia-parent/simi-h5/show /data/tomcat/webapps/simi-h5
 
 
